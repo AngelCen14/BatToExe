cb7d66b40c180b8b56f3175bb4b309f7545c55ef
AngelCen14 <angelcen1447@gmail.com>
2024-10-02 00:24:30 +0200

    Fix bug memoria - Arreglado bug que hacia que se calculase mal la memoria al arreglar el .bat

--- src/bat_processor.c
+++ src/bat_processor.c
@@ -5,32 +5,43 @@
 #include "bat_processor.h"
 
 char *fixBat(const char* batContent) {
-    char *fixedBat = malloc(strlen(batContent) * 2 + 1); 
+    // Calcular el tamaño final
+    size_t finalSize = strlen(batContent);
+    for (size_t i = 0; i < strlen(batContent); i++) {
+        char c = batContent[i];
+        if (c == '\n') finalSize += 4; // " && " = 4 caracteres
+        else if (c == '"') finalSize += 2; // \" = 2 caracteres
+        else if (c == '\\') finalSize += 2; // \\ = 2 caracteres
+    }
+
+    char *fixedBat = malloc(finalSize + 1);
     if (fixedBat == NULL) return NULL;
 
-    fixedBat[0] = '\0';  // Asegurarse de que fixedBat esté vacío
+    fixedBat[0] = '\0';  // Asegurar que fixedBat este vacio
 
     for (size_t i = 0; i < strlen(batContent); i++) {
         char c = batContent[i];
-
-        if (c == '\n') {
-            // Reemplazar nueva línea con ' && ' para concatenar comandos en system()
-            strcat(fixedBat, " && ");
-        } else if (c == '"') {
-            // Escapar las comillas dobles
-            strcat(fixedBat, "\\\"");
-        } else if (c == '\\') {
-            // Escapar barras invertidas
-            strcat(fixedBat, "\\\\");
-        } else {
-            // Añadir el carácter tal cual
-            size_t len = strlen(fixedBat);
-            fixedBat[len] = c;       // Añadir el carácter
-            fixedBat[len + 1] = '\0'; // Asegurar la terminacion de la cadena
+        switch (c) {
+            case '\n':
+                // Reemplazar nueva linea con ' && ' para concatenar comandos en system()
+                strcat(fixedBat, " && ");
+                break;
+            case '"':
+                // Escapar las comillas dobles
+                strcat(fixedBat, "\\\"");
+                break;
+            case '\\':
+                // Escapar barras invertidas
+                strcat(fixedBat, "\\\\");
+                break;
+            default:
+                // Añadir el caracter tal cual
+                size_t len = strlen(fixedBat);
+                fixedBat[len] = c;       // Añadir el caracter
+                fixedBat[len + 1] = '\0'; // Asegurar la terminacion de la cadena
+                break;
         }
     }
-
-    fixedBat = realloc(fixedBat, strlen(fixedBat)+1);
     return fixedBat;
 }
 
